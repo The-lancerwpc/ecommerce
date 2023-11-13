c04f59d736f8dcef0eb4c05914b1273cc3de1318
sidharth2003 <86048378+Hvhjjjbghjjjbbbbvvvvvv@users.noreply.github.com>
2023-11-13 19:18:10 +0530

    mongodb ipv6 localhost alias problem solved

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,13 @@ const connectdatabase=()=>{
     mongoose.connect(process.env.DB_LOCAL_URI, {
         //to resolve deprecation warnings
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        family:4,
+
+        //family 4 bcz for  Enabling the IPV6 that MongoDB has disabled by default.
+        //mongodb://127.0.0.1/test (ipv4)
+        //the localhost alias resolves to IPv6 address
+        //mongodb://localhost:27017/test(ipv6) see in config.env file
      }).then(con=>{
         console.log(`connected with host:${con.connection.host}`)
      })
